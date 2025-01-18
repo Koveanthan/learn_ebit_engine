ed088b55028cb1e0ee23d2fd15a04a51214f69a3
Koveanthan <154501772+Koveanthan@users.noreply.github.com>
2025-01-18 23:40:08 +0530

    add initial commit - template of game engine

---
+++ player.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"image"
+	_ "image/png"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+var playerSprite = mustLoadImage("./assets/png/playerShip1_red.png")
+
+func mustLoadImage(path string) image.Image {
+	f, err := assets.Open(path)
+	if err != nil {
+		panic(err)
+	}
+
+	image, _, err := image.Decode(f)
+	if err != nil {
+		panic(err)
+	}
+
+	return ebiten.NewImageFromImage(image)
+}
