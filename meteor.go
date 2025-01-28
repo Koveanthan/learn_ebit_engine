670480ec05f9e674b1d3eeabe3d044971f2ae15b
Koveanthan <154501772+Koveanthan@users.noreply.github.com>
2025-01-28 22:51:59 +0530

    fix rotation of the individual meteor and vary rotational speed between each

--- meteor.go
+++ meteor.go
@@ -14,48 +14,62 @@ var target = Vector{
 	y: ScreenHeight / 2,
 }
 
+const (
+	rotationSpeedMin = -0.02
+	rotationSpeedMax = 0.02
+	baseVelocity     = 0.25
+)
+
 var radius = ScreenWidth / 2.0
 
-var rotationSpeed = -0.2 + rand.Float64()*0.04
+// var rotationSpeed = -0.2
 
 type Meteor struct {
-	position Vector
-	movement Vector
-	rotation float64
-	sprite   *ebiten.Image
+	position      Vector
+	movement      Vector
+	rotation      float64
+	rotationSpeed float64
+	sprite        *ebiten.Image
 }
 
 func NewMeteor() *Meteor {
 	randAngle := rand.Float64() * math.Pi * 2
-	velocity := 0.25 + rand.Float64()*1.5
+	velocity := baseVelocity + rand.Float64()*1.5
 
+	// position
 	position := Vector{
 		x: target.x + math.Cos(randAngle)*radius,
 		y: target.y + math.Sin(randAngle)*radius,
 	}
+
+	// movement
 	direction := Vector{
 		x: target.x - position.x,
 		y: target.y - position.y,
 	}
-
 	normalizeDirection := direction.Normalize()
-
 	movement := Vector{
 		x: normalizeDirection.x * velocity,
 		y: normalizeDirection.y * velocity,
 	}
+
+	// rotationSpeed
+	rotationSpeed := rotationSpeedMin + rand.Float64()*(rotationSpeedMax-rotationSpeedMin)
+
+	// rotation will be zero by default
 	return &Meteor{
-		position: position,
-		movement: movement,
-		sprite:   meteors[rand.Intn(len(meteors))],
+		position:      position,
+		movement:      movement,
+		rotationSpeed: rotationSpeed,
+		sprite:        meteors[rand.Intn(len(meteors))],
 	}
 }
 
 func (m *Meteor) Update() {
 	m.position.x += m.movement.x
 	m.position.y += m.movement.y
-	m.rotation += rotationSpeed
-	println("Position.x: ", m.position.x, " Position.y: ", m.position.y, " Rotation: ", m.rotation)
+	m.rotation += m.rotationSpeed
+	// println("Position.x: ", m.position.x, " Position.y: ", m.position.y, " Rotation: ", m.rotation)
 }
 
 func (m *Meteor) Draw(screen *ebiten.Image) {
